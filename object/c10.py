0b709ab4e5884021bc9c2d773d7847425a1660a6
roger <qunlin.gu@163.com>
2020-05-05 23:01:35 +0800

    update 2020050502

--- object/c10.py
+++ object/c10.py
@@ -2,7 +2,7 @@
 @Author: Roger
 @Date: 2020-04-23 20:28:58
 @LastEditors: Roger
-@LastEditTime: 2020-05-05 22:57:13
+@LastEditTime: 2020-05-05 23:00:52
 @Email: qunlin.gu@163.com
 @Version: 
 @Description: 
@@ -14,7 +14,7 @@ class Human():
     def __init__(self, name, age):  # 实体方法/构造函数/实例变量
       self.name = name   #self.name = 实例变量
       self.age = age
-      # name = name
+      # name = name  # 这里的name 是类变量,不是实例变量, 实例变量要用self.name
       # age = age
     
     def get_name(self):
