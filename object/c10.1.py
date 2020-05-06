d660fbb3407dd59c42ed59a5bed677094161b621
roger <qunlin.gu@163.com>
2020-05-06 20:45:22 +0800

    update 20200506-1

--- object/c10.1.py
+++ object/c10.1.py
@@ -2,7 +2,7 @@
 @Author: Roger
 @Date: 2020-04-23 20:28:58
 @LastEditors: Roger
-@LastEditTime: 2020-05-05 22:50:59
+@LastEditTime: 2020-05-06 19:20:06
 @Email: qunlin.gu@163.com
 @Version: 
 @Description: 
@@ -10,11 +10,11 @@
 
 class Human():
     
-    sum = 0  # 类变量
+    sum = 10  # 类变量
     def __init__(self, name, age):  # 实体方法/构造函数/实例变量
       self.name = name   #self.name = 实例变量
       self.age = age
-    
+      # print(Human.sum) # 实例方法访问类变量
     def get_name(self):
       print(self.name)
     def do_homework(self):
--- object/c10.py
+++ object/c10.py
@@ -2,7 +2,7 @@
 @Author: Roger
 @Date: 2020-04-23 20:28:58
 @LastEditors: Roger
-@LastEditTime: 2020-05-05 23:00:52
+@LastEditTime: 2020-05-06 19:12:12
 @Email: qunlin.gu@163.com
 @Version: 
 @Description: 
@@ -10,19 +10,21 @@
 
 class Human():
     name = 'peter'  # 类变量
-    sum = 0  # 类变量
-    def __init__(self, name, age):  # 实体方法/构造函数/实例变量
+    sum1 = 0  # 类变量
+    def __init__(self, name, age):  # 实例方法/构造函数/实例变量
       self.name = name   #self.name = 实例变量
       self.age = age
       # name = name  # 这里的name 是类变量,不是实例变量, 实例变量要用self.name
       # age = age
-    
+      print(name) # 这里的name 读取的是形参的name
+      print(self.name)
+      print(self.age)
     def get_name(self):
       print(self.name)
     def do_homework(self):
       print('this is parent method')
 
-student1 = Human('roger',23)
-print(student1.name)
-print(Human.name)
-print(student1.__dict__)
+student1 = Human('roger1',23)
+# print(student1.name)
+# print(Human.name)11
+# print(student1.__dict__)
